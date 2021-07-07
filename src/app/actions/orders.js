5bd360408ecd724572c3d488de7cb72b0389c072
Stanley Sham <stanley.sham94@gmail.com>
2021-07-07 15:18:47 -0400

    switched back to api call

--- src/app/actions/orders.js
+++ src/app/actions/orders.js
@@ -1,25 +1,26 @@
-// import axios from 'axios';
+import axios from 'axios';
 import { 
   GET_DATA,
   APPLY_FILTER,
   CLEAR_FILTER,
 } from './types';
 
-import data from '../../data.json';
+// Import data for heroku in case it has a problem
+// import data from '../../data.json';
 
 // Loads data from API, should only be called once
 export const loadData = () => async (dispatch) => {
   try {
     // Normally would call API, but Heroku is having issues making calls to http links
-    // const response = await axios.post('http://api.interview.staging.foodieorders.com/v3/orders/search', {});
+    const response = await axios.post('http://api.interview.staging.foodieorders.com/v3/orders/search', {});
 
     // Using import data to make it workable on heroku
-    const importedData = data;
-    if (importedData) {
+    // const importedData = data;
+    if (response.data) {
       dispatch({
         type: GET_DATA,
-        // payload: response.data,
-        payload: importedData,
+        payload: response.data,
+        // payload: importedData,
       })
     }
   } catch (err) {
